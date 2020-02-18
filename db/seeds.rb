4f4c71d789c12a722c45f7fefa94c948094b338f
Rui Baltazar <ruivrb@gmail.com>
2020-02-18 18:56:58 +0000

    wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding database'
+Article.destroy_all
+
+puts 'Generating Articles...'
+10.times do
+  Article.create title: Faker::Quote.robin, content: Faker::Quote.matz
+end
+
+puts 'All done! :D'
